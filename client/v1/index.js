5dc3c0f4a837a696ec9547f1dbd13763711f246a
thibaultpkahn <thibaultpkahn@gmail.com>
2025-01-20 14:56:49 +0100

    TODO2

--- client/v1/index.js
+++ client/v1/index.js
@@ -45,6 +45,8 @@ console.log("Best LEGO deal:", bestLegoDeal);
 // 🎯 TODO 2: Number of deals
 // 1. Create a variable and assign it the number of deals
 // 2. Log the variable
+const numberOfDeals = deals.length;
+console.log("Number of deals:", numberOfDeals);
 
 // 🎯 TODO 3: Website name
 // 1. Create a variable and assign it the list of shopping community name only
