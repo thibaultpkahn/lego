c54cd9c9192365587a09c99f681cbf65a14ab011
thibaultpkahn <thibaultpkahn@gmail.com>
2025-01-20 15:01:01 +0100

    TODO3 Shopping communities names

--- client/v1/index.js
+++ client/v1/index.js
@@ -52,6 +52,9 @@ console.log("Number of deals:", numberOfDeals);
 // 1. Create a variable and assign it the list of shopping community name only
 // 2. Log the variable
 // 3. Log how many shopping communities we have
+const shoppingCommunityNames = deals.map(deal => deal.name);
+console.log("Shopping communities:", shoppingCommunityNames);
+console.log("Number of shopping communities:", shoppingCommunityNames.length);
 
 // 🎯 TODO 4: Sort by price
 // 1. Create a function to sort the deals by price
