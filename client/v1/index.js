5b966d3a954ca4dc3467d5e5808eb918e89ae635
thibaultpkahn <thibaultpkahn@gmail.com>
2025-01-20 14:47:23 +0100

    test 1

--- client/v1/index.js
+++ client/v1/index.js
@@ -29,6 +29,9 @@ console.log(MY_FAVORITE_DEALERS[0]);
 // 0. I have 2 favorite lego sets shopping communities stored in MY_FAVORITE_DEALERS variable
 // 1. Create a new variable and assign it the link of the lego set with the highest reduction I can find on these 2 websites
 // 2. Log the variable
+const bestLegoDeal = "https://www.dealabs.com/bons-plans/selection-lego-chez-cultura-ex-lego-super-mario-71387-2979327";
+console.log("Best LEGO deal:", bestLegoDeal);
+
 
 /**
  * 🧱
