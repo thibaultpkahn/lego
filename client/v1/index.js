0a0166c4e21d896aaf6a8b21d6792b9affe2683c
thibaultpkahn <thibaultpkahn@gmail.com>
2025-01-27 14:47:19 +0100

    hh

--- client/v1/index.js
+++ client/v1/index.js
@@ -495,9 +495,9 @@ console.log("Item with UUID:", foundItem ? foundItem : "Not found");
 // 🎯 TODO 14: Delete a specific item
 // 1. Delete the item with the uuid `f2c5377c-84f9-571d-8712-98902dcbb913`
 // 2. Log the new list of items
-VINTED = VINTED.filter(item => item.uuid !== "f2c5377c-84f9-571d-8712-98902dcbb913");
-console.log(VINTED);
-console.table(VINTED);
+vinted = VINTED.filter(item => item.uuid !== "f2c5377c-84f9-571d-8712-98902dcbb913");
+console.log(vinted);
+console.table(vinted);
 
 
 // 🎯 TODO 5: Save a favorite item
